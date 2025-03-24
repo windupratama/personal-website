f116586a0c477d9fef9a2ac8ca0c19e68a134073
windupratama <winduprtma@gmail.com>
2025-03-24 14:02:31 +0700

    feat(underline): add underline hover component

---
+++ src/components/ui/underline-hover.tsx
@@ -0,0 +1,29 @@
+/**
+ * a component that provide a underline effect on it's children when hovered (width based on children)
+ **/
+
+import { cn } from "@/utils/cn";
+
+interface UnderlineHoverProps extends React.HTMLAttributes<HTMLDivElement> {
+    children: React.ReactNode;
+}
+
+function UnderlineHover({
+    children,
+    className,
+    ...props
+}: UnderlineHoverProps) {
+    return (
+        <div
+            className={cn(
+                "relative h-fit w-fit cursor-pointer before:absolute before:bottom-0 before:left-0 before:h-0.5 before:w-0 before:bg-current before:transition-all before:duration-300 hover:before:w-full",
+                className
+            )}
+            {...props}
+        >
+            {children}
+        </div>
+    );
+}
+
+export { UnderlineHover };
